e9a92fc086915e97933ce7acb26900479b79daec
Gabriela De Carlos <gabydecarlos260@gmail.com>
2023-10-01 20:42:43 -0300

    Navegando entre templates

--- AppCoder/urls.py
+++ AppCoder/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from .views import inicio,cursos,profesores, estudiantes, entregables
 
 urlpatterns = [
-    path('inicio/', inicio),
-    path('cursos/', cursos),
-    path('profesores/', profesores),
+    path('inicio/', inicio, name= "Inicio"),
+    path('cursos/', cursos, name= "Cursos"),
+    path('profesores/', profesores, name= "Profesores"),
     path('estudiantes/',estudiantes, name="Estudiantes"),
-    path('entregables/', entregables)
+    path('entregables/', entregables, name= "Entregables")
 ]
